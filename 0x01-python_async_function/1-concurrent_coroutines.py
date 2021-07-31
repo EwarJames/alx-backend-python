6a5883faa857b91a91169b5b1055c13dacec6ae2
EwarJames <ewaarjames@gmail.com>
2021-07-31 11:42:14 +0000

    Concurrent coroutine

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Executing multiple coroutines at the same time with async
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax.py').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Async routine that takes two arguments and returns
+    the lists of all the delays (float values). The list
+    are in ascending order without using sort
+    """
+    initial_delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        initial_delays.append(wait_random(max_delay))
+    for initial_delay in asyncio.as_completed(initial_delays):
+        initial = await delay
+        all_delays.append(initial)
+    return all_delays
